5e8b5178f569880d740da783c57fa6f998e4abe5
Phong Lo <phonglo724@gmail.com>
2021-05-26 21:26:46 -0600

    installed jwt and login method for part 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,17 @@ class UsersController < ApplicationController
         render json: @user, status: :created
     end
 
+    def login 
+        @user = User.find_by(
+            username: params[:username]
+        )
+        if @user && @user.authenticate(params[:password])
+            @token = JWT.encode({user_id: @user.id}, 'secret')
+
+            render json: {token: @token}
+        else
+            render json: {error: "Invalid Credentials"}, status: :unauthorized
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post "users", to: "users#create"
+  post "/login", to: "users#login"
 end
